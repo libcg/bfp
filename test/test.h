7c50016135522a903128ec222d88729923842bf1
Clément Guérin <libcg@protonmail.com>
2017-02-23 01:19:52 -0500

    test: use NAN macro

--- test/test.h
+++ test/test.h
@@ -1,6 +1,8 @@
 #ifndef __TEST_H
 #define __TEST_H
 
+#define NAN     (-1)
+
 #define TEST_OP2(OP, NBITS, ES)                                                \
     Posit a = Posit(NBITS, ES);                                                \
     Posit b = Posit(NBITS, ES);                                                \
@@ -12,7 +14,7 @@
                                                                                \
             Posit c = a.OP(b);                                                 \
                                                                                \
-            if (OP ## _table[j][i] == -1) {                                    \
+            if (OP ## _table[j][i] == NAN) {                                   \
                 CuAssertTrue(tc, c.isNan());                                   \
             } else {                                                           \
                 CuAssertTrue(tc, !c.isNan());                                  \
