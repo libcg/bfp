173ce2baf1fa37f03f95190618f4627bba5ca0fc
Clément Guérin <libcg@protonmail.com>
2017-02-23 00:17:12 -0500

    test: add tests for isAbc() methods

--- test/p2_test.cpp
+++ test/p2_test.cpp
@@ -101,6 +101,40 @@ static void TestP2Div(CuTest* tc)
     TEST_OP2(div)
 }
 
+static void TestP2Is(CuTest* tc)
+{
+    Posit p = Posit(2, 0);
+    Posit zero = p.zero();
+    Posit one = p.one();
+    Posit inf = p.inf();
+    Posit mone = p.one().neg();
+
+    CuAssertTrue(tc, zero.isZero());
+    CuAssertTrue(tc, !one.isZero());
+    CuAssertTrue(tc, !inf.isZero());
+    CuAssertTrue(tc, !mone.isZero());
+
+    CuAssertTrue(tc, !zero.isOne());
+    CuAssertTrue(tc, one.isOne());
+    CuAssertTrue(tc, !inf.isOne());
+    CuAssertTrue(tc, mone.isOne());
+
+    CuAssertTrue(tc, !zero.isInf());
+    CuAssertTrue(tc, !one.isInf());
+    CuAssertTrue(tc, inf.isInf());
+    CuAssertTrue(tc, !mone.isInf());
+
+    CuAssertTrue(tc, !zero.isNeg());
+    CuAssertTrue(tc, !one.isNeg());
+    CuAssertTrue(tc, !inf.isNeg());
+    CuAssertTrue(tc, mone.isNeg());
+
+    CuAssertTrue(tc, !zero.isNan());
+    CuAssertTrue(tc, !one.isNan());
+    CuAssertTrue(tc, !inf.isNan());
+    CuAssertTrue(tc, !mone.isNan());
+}
+
 CuSuite* TestP2GetSuite(void)
 {
 	CuSuite* suite = CuSuiteNew();
@@ -110,6 +144,7 @@ CuSuite* TestP2GetSuite(void)
 	SUITE_ADD_TEST(suite, TestP2Sub);
 	SUITE_ADD_TEST(suite, TestP2Mul);
 	SUITE_ADD_TEST(suite, TestP2Div);
+	SUITE_ADD_TEST(suite, TestP2Is);
 
 	return suite;
 }
