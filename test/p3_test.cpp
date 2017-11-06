19645912ee48d0ef3fe5ccd400a1d9f518de072d
Clément Guérin <libcg@protonmail.com>
2017-11-05 21:16:17 -0800

    test: add P3Add/Sub tests

--- test/p3_test.cpp
+++ test/p3_test.cpp
@@ -6,6 +6,19 @@
 
 #include "test.h"
 
+static POSIT_STYPE sub_table[8][8];
+static POSIT_STYPE add_table[8][8] = {
+//        0    1/4      1      4    inf     -4     -1   -1/4
+    { 0b000, 0b001, 0b010, 0b011, 0b100, 0b101, 0b110, 0b111 }, // 0
+    { 0b001, 0b001, 0b010, 0b011, 0b100, 0b101, 0b110, 0b000 }, // 1/4
+    { 0b010, 0b010, 0b010, 0b011, 0b100, 0b101, 0b000, 0b010 }, // 1
+    { 0b011, 0b011, 0b011, 0b011, 0b100, 0b000, 0b011, 0b011 }, // 4
+    { 0b100, 0b100, 0b100, 0b100,   NAN, 0b100, 0b100, 0b100 }, // inf
+    { 0b101, 0b101, 0b101, 0b000, 0b100, 0b101, 0b101, 0b101 }, // -4
+    { 0b110, 0b110, 0b000, 0b011, 0b100, 0b101, 0b110, 0b110 }, // -1
+    { 0b111, 0b000, 0b010, 0b011, 0b100, 0b101, 0b110, 0b111 }, // -1/4
+};
+
 static POSIT_STYPE div_table[8][8];
 static POSIT_STYPE mul_table[8][8] = {
 //        0    1/4      1      4    inf     -4     -1   -1/4
@@ -18,6 +31,16 @@ static POSIT_STYPE mul_table[8][8] = {
     { 0b000, 0b111, 0b110, 0b101, 0b100, 0b011, 0b010, 0b001 }, // -1
     { 0b000, 0b111, 0b111, 0b110, 0b100, 0b010, 0b001, 0b001 }, // -1/4
 };
+
+static void TestP3Add(CuTest* tc)
+{
+    TEST_OP2(add, 3, 1)
+}
+
+static void TestP3Sub(CuTest* tc)
+{
+    TEST_OP2(sub, 3, 1)
+}
 
 static void TestP3Mul(CuTest* tc)
 {
@@ -33,8 +56,11 @@ CuSuite* TestP3GetSuite(void)
 {
     CuSuite* suite = CuSuiteNew();
 
+    genSubTable(sub_table[0], add_table[0], 8);
     genDivTable(div_table[0], mul_table[0], 8);
 
+    SUITE_ADD_TEST(suite, TestP3Add);
+    SUITE_ADD_TEST(suite, TestP3Sub);
     SUITE_ADD_TEST(suite, TestP3Mul);
     SUITE_ADD_TEST(suite, TestP3Div);
 
